ef2c6bfe53a56bbcc0d8fa3bc2ad6070cf29d75d
Kyler Hamilton <tech@kyler.me>
2014-10-21 10:36:15 -0400

    Updates to sharing, form, and form confirmation

--- js/action.js
+++ js/action.js
@@ -1,14 +1,29 @@
-$(function() {
-    $('#action_form').bind('submit', function(e){
-        var form = $(this);
-        var buttons = form.find('input[type=submit]');
+ $(function() {
 
-        buttons.attr('disabled', 'disabled').text('Working...');
+        $('#action_form').bind('submit', function(e){
+            var form = $(this);
+            var buttons = form.find('button[type=submit]');
 
-        $.post(form.attr('action'), form.serialize(), function(data){
-            alert('thanks!')
-        });
 
-        e.preventDefault();
-    });
-})
+            buttons.attr('disabled', 'disabled').text('Working...');
+            
+            $.post(form.attr('action'), form.serialize(), function(data){
+                $action_widget = document.getElementById('action_widget');
+                $action_widget.className = ( $action_widget.className == 'share-widget' )? '' : 'share-widget';
+            });
+
+            e.preventDefault(); 
+        }); 
+        
+
+});
+
+
+$(document).ready(function() {
+     $('button[type="submit"]').attr('disabled','disabled');
+     $('input[type="text"]').keyup(function() {
+        if( $('#input-name').val() != '' && $('#input-email').val() != '')  {
+           $('button[type="submit"]').removeAttr('disabled');
+          }
+       });
+     });
